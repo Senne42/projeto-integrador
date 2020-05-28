008401c3ca3feda3d3be92bfe944b82888b84267
Luis <luisaugustosb93@gmail.com>
2020-05-28 18:42:40 -0300

    Tela de cadastro de eventos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+  titulo = '';
+  descricao = '';
+  data = '';
+  eventos = [];
+  onSubmit(){
+    this.eventos.push({
+      "titulo": this.titulo,
+      "descricao": this.descricao,
+      "data": this.data
+    });
+    console.log(this.eventos);
+    console.log(this.eventos[0].titulo);
+  }
 
 }
