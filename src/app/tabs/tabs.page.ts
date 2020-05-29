40831139a5b7d720e8718c70f58c9e44f8143ba4
Senne42 <mateussenne@hotmail.com>
2020-05-29 18:58:14 -0300

    primeiros comandos de voz

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  matches: String[];
+  constructor(private speechRecognition: SpeechRecognition, private cd: ChangeDetectorRef, private router: Router,) {}
 
-  constructor() {}
+  microfone(){  
+    let options = {
+      language: 'pt-BR'
+    }
+    this.speechRecognition.startListening().subscribe(matches => {
+      this.matches = matches;
+      for (let i = 0; this.matches.length > i; i++){
+        if (this.matches[i].toLowerCase().includes("abrir calendário")){
+          this.router.navigate(['tabs/tab3']);
+        }
+        if (this.matches[i].toLowerCase().includes("adicionar lembrete")){
+          this.router.navigate(['tabs/tab2']);
+        }
+    }
+      this.cd.detectChanges();
+    });
+  }
 
 }
