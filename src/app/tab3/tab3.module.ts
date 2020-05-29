430e1f1cb15e3f9c29823a759183a1d7e410344b
Senne42 <mateussenne@hotmail.com>
2020-05-29 18:30:25 -0300

    correção do calendário

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -7,6 +7,7 @@ import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module'
+import { NgCalendarModule  } from 'ionic2-calendar';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module'
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    NgCalendarModule,
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,122 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  eventSource;
+  viewTitle;
 
-  constructor() {}
+  isToday:boolean;
+  calendar = {
+      mode: 'month',
+      currentDate: new Date(),
+      dateFormatter: {
+          formatMonthViewDay: function(date:Date) {
+              return date.getDate().toString();
+          },
+          formatMonthViewDayHeader: function(date:Date) {
+              return 'MonMH';
+          },
+          formatMonthViewTitle: function(date:Date) {
+              return 'testMT';
+          },
+          formatWeekViewDayHeader: function(date:Date) {
+              return 'MonWH';
+          },
+          formatWeekViewTitle: function(date:Date) {
+              return 'testWT';
+          },
+          formatWeekViewHourColumn: function(date:Date) {
+              return 'testWH';
+          },
+          formatDayViewHourColumn: function(date:Date) {
+              return 'testDH';
+          },
+          formatDayViewTitle: function(date:Date) {
+              return 'testDT';
+          }
+      }
+  };
 
+  constructor(private navController:NavController) {
+
+  }
+
+  loadEvents() {
+      this.eventSource = this.createRandomEvents();
+  }
+
+  onViewTitleChanged(title) {
+      this.viewTitle = title;
+  }
+
+  onEventSelected(event) {
+      console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  changeMode(mode) {
+      this.calendar.mode = mode;
+  }
+
+  today() {
+      this.calendar.currentDate = new Date();
+  }
+
+  onTimeSelected(ev) {
+      console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+          (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+  }
+
+  onCurrentDateChanged(event:Date) {
+      var today = new Date();
+      today.setHours(0, 0, 0, 0);
+      event.setHours(0, 0, 0, 0);
+      this.isToday = today.getTime() === event.getTime();
+  }
+
+  createRandomEvents() {
+      var events = [];
+      for (var i = 0; i < 50; i += 1) {
+          var date = new Date();
+          var eventType = Math.floor(Math.random() * 2);
+          var startDay = Math.floor(Math.random() * 90) - 45;
+          var endDay = Math.floor(Math.random() * 2) + startDay;
+          var startTime;
+          var endTime;
+          if (eventType === 0) {
+              startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+              if (endDay === startDay) {
+                  endDay += 1;
+              }
+              endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+              events.push({
+                  title: 'All Day - ' + i,
+                  startTime: startTime,
+                  endTime: endTime,
+                  allDay: true
+              });
+          } else {
+              var startMinute = Math.floor(Math.random() * 24 * 60);
+              var endMinute = Math.floor(Math.random() * 180) + startMinute;
+              startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+              endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+              events.push({
+                  title: 'Event - ' + i,
+                  startTime: startTime,
+                  endTime: endTime,
+                  allDay: false
+              });
+          }
+      }
+      return events;
+  }
+
+  onRangeChanged(ev) {
+      console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+  }
+
+  markDisabled = (date:Date) => {
+      var current = new Date();
+      current.setHours(0, 0, 0);
+      return date < current;
+  };
 }
+
