776f270198079d5abbbab2da315728af643a81e1
Larissa Costa <larissacto99@gmail.com>
2020-06-19 19:22:59 -0300

    Criação de Tela Lembretes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'lembretes',
+    loadChildren: () => import('./lembretes/lembretes.module').then( m => m.LembretesPageModule)
   }
+
 ];
 @NgModule({
   imports: [
